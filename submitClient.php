a195cfa64dbac9c0707e2f54258290a6df9ce9c0
Sasa <alyssa.benitez@obf.ateneo.edu>
2016-05-14 16:12:40 +0800

    WORKING CLIENT TO CASE REDIRECTION

--- submitClient.php
+++ submitClient.php
@@ -28,11 +28,23 @@
 		}
 		else{
 			//JOKE LANG
-			$id = mysqli_insert_id();
-			echo('<meta http-equiv="refresh" content="0;URL=addCase.php?value='.$id.'"/>');
-		}		
+			// $id = mysqli_insert_id();
+			//KELANGAN KO MAKUHA YUNG CLIENT ID PAGKAINSERT
+			$sql2="SELECT client_id FROM clients WHERE account_id='$userid' AND classification='$class' AND representative_first_name='$firstr' AND representative_last_name='$lastr' AND comaker_first_name='$firstc' AND comaker_last_name='$lastc' AND company_name='$company' AND address='$oadd' AND status='$status' AND email='$email' AND contact_number='$tel' AND notes='$notes'";
+			$result2 = mysqli_query($conn, $sql2);
+			  if (mysqli_num_rows($result2) > 0) 
+			  {
+			    while($row = mysqli_fetch_assoc($result2)) 
+			    {
+				echo('<meta http-equiv="refresh" content="0;URL=addCase.php?value='.$row['client_id'].'"/>');
+				}
+			}
+			else{
+				echo $conn->error;
+			}		
+		}
 	}
 	else{
-		echo "fuq";
+		echo $conn->error;
 	}
 ?>
