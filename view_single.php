3bf5bcde74dedab63e288fd71154e65f02400ade
Sasa <alyssa.benitez@obf.ateneo.edu>
2016-05-14 16:18:24 +0800

    css fixes c/o anne

--- view_single.php
+++ view_single.php
@@ -18,8 +18,8 @@ include ('database.php');
             if (mysqli_num_rows($result) > 0) {
                 while($row = mysqli_fetch_assoc($result)) {
     ?>
-        	<h1><?php echo $row['representative_last_name'].", ".$row['representative_first_name'];?></h1>
-            <h3>(<?php echo $row['status'];?>)</h3>
+        	<h1 id="single_client_name"><?php echo $row['representative_last_name'].", ".$row['representative_first_name'];?></h1>
+            <h3 id="single_client_status">(<?php echo $row['status'];?>)</h3>
 
     <?php
         		}
@@ -41,26 +41,26 @@ include ('database.php');
                 while($row = mysqli_fetch_assoc($result1)) {
         ?>
 	        	<tr>
-			      <td>Classification</td>
-			      <td><?php echo $row['classification']?></td>
-			      <td>E-mail</td>
-			      <td><?php echo $row['email']?></td>
+			      <td id="single_details">Classification</td>
+			      <td id="single_spec_details"><?php echo $row['classification']?></td>
+			      <td id="single_details">E-mail</td>
+			      <td id="single_spec_details"><?php echo $row['email']?></td>
 			    </tr>
 			    <tr>
-			    	<td>Co-Maker</td>
-			    	<td><?php echo $row['comaker_last_name'].", ".$row['comaker_first_name']?></td>
-			    	<td>Contact Number</td>
-			    	<td><?php echo $row['contact_number']?></td>
+			    	<td id="single_details">Co-Maker</td>
+			    	<td id="single_spec_details"><?php echo $row['comaker_last_name'].", ".$row['comaker_first_name']?></td>
+			    	<td id="single_details">Contact Number</td>
+			    	<td id="single_spec_details"><?php echo $row['contact_number']?></td>
 			    </tr>
 			    <tr>
-			    	<td>Company Name</td>
-			    	<td><?php echo $row['company_name']?></td>
-			    	<td>Notes</td>
-			    	<td rowspan="3">Hello World</td>
+			    	<td id="single_details">Company Name</td>
+			    	<td id="single_spec_details"><?php echo $row['company_name']?></td>
+			    	<td id="single_details">Notes</td>
+			    	<td rowspan="3" id="single_spec_details">Hello World</td>
 			    </tr>
 			    <tr>
-			    	<td>Address</td>
-			    	<td rowspan="2">1211 Katipunan Ave., Quezon City</td>
+			    	<td id="single_details">Address</td>
+			    	<td rowspan="2" id="single_spec_details">1211 Katipunan Ave., Quezon City</td>
 			    </tr>
 			    <tr></tr>
         <?php
@@ -69,10 +69,12 @@ include ('database.php');
         ?>
 		</table>
 	</div>
+	<br>
+	<br>
 
 <!-- div for active case header with loan duration -->
 <div>
-	<h3>ACTIVE CASE</h3>
+	<h3 id="single_client_status">ACTIVE CASE</h3>
 
 	<?php
 		if (isset($_GET['url_id'])){
@@ -97,25 +99,25 @@ include ('database.php');
 	</div>
 
 <!-- div for expected table -->
-<div class="table-responsive" id="view_expected_table">
-	<table class="table">
+<div class="table-responsive" id="div_tables">
+	<table class="table" id="expected_table">
 		<thead>
 		  	<tr>
-		  		<td colspan="8" id="colored_head">EXPECTED</td>
+		  		<td colspan="8" id="single_heads">EXPECTED</td>
 		  	</tr>
 		  	<tr>
-		  		<td rowspan="2">No.</td>
-		  		<td rowspan="2">Due Date</td>
-		  		<td colspan="3">Due</td>
-		  		<td colspan="3">Balance</td>
+		  		<td rowspan="2" id="center_due">No.</td>
+		  		<td rowspan="2" id="dates">Due Date</td>
+		  		<td colspan="3" id="center_due">Due</td>
+		  		<td colspan="3" id="center_balance">Balance</td>
 		  	</tr>
 		  	<tr>
-		  		<td>Principal</td>
-		  		<td>Interest</td>
-		  		<td>Total</td>
-		  		<td>Principal</td>
-		  		<td>Interest</td>
-		  		<td>Total</td>
+		  		<td id="center_due">Principal</td>
+		  		<td id="center_due">Interest</td>
+		  		<td id="center_due">Total</td>
+		  		<td id="center_balance">Principal</td>
+		  		<td id="center_balance">Interest</td>
+		  		<td id="center_balance">Total</td>
 		  	</tr>
 		</thead>
 		<tbody>
@@ -126,19 +128,19 @@ include ('database.php');
 		if (isset($_GET['url_id'])){
 		  	$local_id=$_GET['url_id'];
 
-            $sql3 = "SELECT c.date_of_release, c.loan_amount, c.weekly_interest_rate, c.payment_period, (c.loan_amount*c.weekly_interest_rate*0.01) intBal, (c.loan_amount+(c.loan_amount*c.weekly_interest_rate*0.01)) totalBal FROM cases c WHERE c.client_id=$local_id;";
+            $sql3 = "SELECT DATE_FORMAT(c.date_of_release, '%b-%d-%y') AS dr, c.loan_amount, c.weekly_interest_rate, c.payment_period, (c.loan_amount*c.weekly_interest_rate*0.01) intBal, (c.loan_amount+(c.loan_amount*c.weekly_interest_rate*0.01)) totalBal FROM cases c WHERE c.client_id=$local_id;";
             $result3 = mysqli_query($conn, $sql3);;
 
             if (mysqli_num_rows($result3) > 0) {
                 while($row = mysqli_fetch_assoc($result3)) {
        	?>
         	<tr>
-        		<td class="container" id="center_column">Release</td>
-        		<td class="container" id="center_column"><?php echo $row['date_of_release']?></td>
-        		<td colspan="3"></td>
-        		<td class="container" id="money"><?php echo $row['loan_amount']?></td>
-                <td class="container" id="money"><?php $forIntBal = number_format($row['intBal'], 2); echo $forIntBal;?></td>
-                <td class="container" id="money"><?php $totIntBal = number_format($row['totalBal'], 2); echo $totIntBal;?></td>
+        		<td class="container" id="center_due">Release</td>
+        		<td class="container" id="dates"><?php echo $row['dr']?></td>
+        		<td colspan="3" id="single_due"></td>
+        		<td class="container" id="single_balance"><?php echo $row['loan_amount']?></td>
+                <td class="container" id="single_balance"><?php $forIntBal = number_format($row['intBal'], 2); echo $forIntBal;?></td>
+                <td class="container" id="single_balance"><?php $totIntBal = number_format($row['totalBal'], 2); echo $totIntBal;?></td>
             </tr>
         <?php
         		}
@@ -158,14 +160,14 @@ include ('database.php');
                 while($row = mysqli_fetch_assoc($result4)) {
         ?>
         	<tr>
-        		<td class="container" id="center_column"><?php echo $i; $i++; ?></td>
-        		<td class="container" id="center_column"><?php echo $row['dd']?></td>
-        		<td class="container" id="money"><?php echo $row['principal_due']?></td>
-                <td class="container" id="money"><?php echo $row['interest_due']?></td>
-                <td class="container" id="money"><?php echo $row['total_due']?></td>
-                <td class="container" id="money"><?php echo $row['expected_principal_balance']?></td>
-                <td class="container" id="money"><?php echo $row['expected_interest_balance']?></td>
-                <td class="container" id="money"><?php echo $row['expected_total_balance']?></td>
+        		<td class="container" id="center_due"><?php echo $i; $i++; ?></td>
+        		<td class="container" id="dates"><?php echo $row['dd']?></td>
+        		<td class="container" id="single_due"><?php echo $row['principal_due']?></td>
+                <td class="container" id="single_due"><?php echo $row['interest_due']?></td>
+                <td class="container" id="single_due"><?php echo $row['total_due']?></td>
+                <td class="container" id="single_balance"><?php echo $row['expected_principal_balance']?></td>
+                <td class="container" id="single_balance"><?php echo $row['expected_interest_balance']?></td>
+                <td class="container" id="single_balance"><?php echo $row['expected_total_balance']?></td>
             </tr>
         <?php
     			}
@@ -175,23 +177,23 @@ include ('database.php');
 	</table>
 
 	<!-- payments -->
-	<table class="table">
+	<table class="table" id="payment_table">
 		<thead>
 		  	<tr>
-		  		<td colspan="7" id="colored_head">PAYMENTS</td>
+		  		<td colspan="7" id="single_heads">PAYMENTS</td>
 		  	</tr>
 		  	<tr>
-		  		<td rowspan="2">Turn Date</td>
-		  		<td rowspan="2">Turn Amount</td>
-		  		<td colspan="2">Paid</td>
-		  		<td colspan="3">Balance</td>
+		  		<td rowspan="2" id="dates">Turn Date</td>
+		  		<td colspan="3" id="center_due">Paid</td>
+		  		<td colspan="3" id="center_balance">Balance</td>
 		  	</tr>
 		  	<tr>
-		  		<td>Principal</td>
-		  		<td>Interest</td>
-		  		<td>Principal</td>
-		  		<td>Interest</td>
-		  		<td>Total</td>
+		  		<td id="center_due">Principal</td>
+		  		<td id="center_due">Interest</td>
+		  		<td id="center_due">Turn Amount</td>
+		  		<td id="center_balance">Principal</td>
+		  		<td id="center_balance">Interest</td>
+		  		<td id="center_balance">Total</td>
 		  	</tr>
 		</thead>
 		<tbody>
@@ -207,13 +209,13 @@ include ('database.php');
                 while($row = mysqli_fetch_assoc($result5)) {
         ?>
         	<tr>
-        		<td class="container" id="center_column"><?php echo $row['turn_date']?></td>
-        		<td class="container" id="money"><?php echo $row['turn_amount']?></td>
-        		<td class="container" id="money"><?php echo $row['principal_paid']?></td>
-                <td class="container" id="money"><?php echo $row['interest_paid']?></td>
-                <td class="container" id="money"><?php echo $row['actual_principal_balance']?></td>
-                <td class="container" id="money"><?php echo $row['actual_interest_balance']?></td>
-                <td class="container" id="money"><?php echo $row['actual_total_balance']?></td>
+        		<td class="container" id="dates"><?php echo $row['turn_date']?></td>
+        		<td class="container" id="single_due"><?php echo $row['principal_paid']?></td>
+                <td class="container" id="single_due"><?php echo $row['interest_paid']?></td>
+        		<td class="container" id="single_due"><?php echo $row['turn_amount']?></td>
+                <td class="container" id="single_balance"><?php echo $row['actual_principal_balance']?></td>
+                <td class="container" id="single_balance"><?php echo $row['actual_interest_balance']?></td>
+                <td class="container" id="single_balance"><?php echo $row['actual_total_balance']?></td>
             </tr>
         <?php
         		}
@@ -226,7 +228,8 @@ include ('database.php');
 
 <!-- div for finished cases -->
 <div>
-	<h3>FINISHED CASES</h3>
+	<h3 id="single_client_status">FINISHED CASES</h3>
+	<br>
 	No finished cases.
 	<!-- INSERT CODE HERE -->
 </div>
