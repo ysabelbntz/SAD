41d7d02b9f9bcf42fedee1e23e067dd75867a639
Sasa <alyssa.benitez@obf.ateneo.edu>
2016-05-17 05:49:02 +0800

    1

--- database.php
+++ database.php
@@ -1,22 +1,15 @@
- <?php
+<?php
 
 
-// $hostname = "us-cdbr-iron-east-04.cleardb.net";
-// $username = "b248e13104fe12";
-// $password = "56e753b9";
-// $database = "heroku_0d16bc3ff328247";
+$hostname = "us-cdbr-iron-east-04.cleardb.net";
+$username = "b248e13104fe12";
+$password = "56e753b9";
+$database = "heroku_0d16bc3ff328247";
 
-// $conn = new mysqli ($hostname, $username, $password, $database);
+$conn = new mysqli ($hostname, $username, $password, $database);
 
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-
-$url = parse_url(getenv('CLEARDB_DATABASE_URL'));
-
-echo '<pre>';
-var_dump( $url);
-echo '</pre>';
-die;
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 ?>
