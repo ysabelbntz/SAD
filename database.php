94d71e83fae62c5d352f1cd55a31d98d192d6108
Sasa <alyssa.benitez@obf.ateneo.edu>
2016-05-17 05:29:18 +0800

    huhu

--- database.php
+++ database.php
@@ -2,13 +2,13 @@
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$hostname = $url["us-cdbr-iron-east-04.cleardb.net"];
-$username = $url["b248e13104fe12"];
-$password = $url["56e753b9"];
-$database = $url["heroku_0d16bc3ff328247"];
+$hostname = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["heroku_0d16bc3ff328247"], 1);
 
 $conn = new mysqli ($hostname, $username, $password);
-$conn->select_db($database);
+$conn->select_db($db);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
