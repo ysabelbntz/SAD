e5a17022efda5c4c0f92db2a2a84d1dda8866f9e
Sasa <alyssa.benitez@obf.ateneo.edu>
2016-05-17 03:35:04 +0800

    trial

--- database.php
+++ database.php
@@ -1,11 +1,15 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$db = "mega";
+
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['root'];
+$password = $dbparts['sasabenitez'];
+$database = ltrim($dbparts['mega'],'/');
 
 $conn = new mysqli ($servername, $username, $password);
-$conn->select_db($db);
+$conn->select_db($database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
