012c72b4a0145899563218302266a0ffc11f9c66
Sasa <alyssa.benitez@obf.ateneo.edu>
2016-05-17 05:13:37 +0800

    trials

--- database.php
+++ database.php
@@ -1,14 +1,13 @@
 <?php
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $hostname = $url["us-cdbr-iron-east-04.cleardb.net"];
 $username = $url["b248e13104fe12"];
 $password = $url["56e753b9"];
 $database = $url["heroku_0d16bc3ff328247"];
 
-$conn = new mysqli ($servername, $username, $password);
-$conn->select_db($database);
+$conn = new mysqli ($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
