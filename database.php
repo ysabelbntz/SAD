364c298d4923f95eec2ed72d7e529243e49f5c15
Sasa <alyssa.benitez@obf.ateneo.edu>
2016-05-17 11:02:15 +0800

    trial

--- database.php
+++ database.php
@@ -8,8 +8,7 @@ $username = "b248e13104fe12";
 $password = "56e753b9";
 $database = "heroku_0d16bc3ff328247";
 
-$conn = new mysqli ($hostname, $username, $password);
-$conn->select_db($database);
+$conn = mysqli_connect($hostname,$username,$password,$database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
