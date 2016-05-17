9f2612a560fa6e6f6e10d292738540162e105284
Sasa <alyssa.benitez@obf.ateneo.edu>
2016-05-17 11:39:36 +0800

    back to normal

--- database.php
+++ database.php
@@ -3,22 +3,10 @@
 
 // $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$hostname = "us-cdbr-iron-east-04.cleardb.net";
-$username = "b248e13104fe12";
-$password = "56e753b9";
-$database = "heroku_0d16bc3ff328247";
-
-$conn = new mysqli ($hostname, $username, $password);
-$conn->select_db($database);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// $hostname = "localhost";
-// $username = "root";
-// $password = "sasabenitez";
-// $database = "mega";
+// $hostname = "us-cdbr-iron-east-04.cleardb.net";
+// $username = "b248e13104fe12";
+// $password = "56e753b9";
+// $database = "heroku_0d16bc3ff328247";
 
 // $conn = new mysqli ($hostname, $username, $password);
 // $conn->select_db($database);
@@ -26,5 +14,17 @@ if ($conn->connect_error) {
 // if ($conn->connect_error) {
 //     die("Connection failed: " . $conn->connect_error);
 // }
+
+$hostname = "localhost";
+$username = "root";
+$password = "sasabenitez";
+$database = "mega";
+
+$conn = new mysqli ($hostname, $username, $password);
+$conn->select_db($database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 ?>
