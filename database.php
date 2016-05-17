e5019d50e75be373d290c1211cbc30167dc78504
Sasa <alyssa.benitez@obf.ateneo.edu>
2016-05-17 11:20:58 +0800

    ayaw pa din

--- database.php
+++ database.php
@@ -3,10 +3,10 @@
 
 // $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$hostname = "us-cdbr-iron-east-04.cleardb.net";
-$username = "b248e13104fe12";
-$password = "56e753b9";
-$database = "heroku_0d16bc3ff328247";
+$hostname = 'us-cdbr-iron-east-04.cleardb.net';
+$username = 'b248e13104fe12';
+$password = '56e753b9';
+$database = 'heroku_0d16bc3ff328247';
 
 $conn = new mysqli ($hostname,$username,$password);
 $conn->select_db($database);
