237ef2e3fbd0ce48ee1a785f07d0a627738fe558
imrobberting <robert.cuartero@obf.ateneo.edu>
2016-08-07 17:07:06 +0800

    Converted sql queries to parameterized in submitClient.php

--- submitClient.php
+++ submitClient.php
@@ -3,46 +3,47 @@
 	session_start();
 	if(isset($_POST['add_button'])) {
 
-		$class=$_POST['class'];
-		$lastr=$_POST['lastr'];
-		$firstr=$_POST['firstr'];
-		$lastc=$_POST['lastc'];
-		$firstc=$_POST['firstc'];
-		$company=$_POST['company'];
-		$oadd=$_POST['oadd'];
-		$tel=$_POST['tel'];
-		$email=$_POST['email'];
-		$status=$_POST['status'];
-		$notes=$_POST['notes'];
+			$userid = $_SESSION['id'];
+			$class=$_POST['class'];
+			$lastr=$_POST['lastr'];
+			$firstr=$_POST['firstr'];
+			$lastc=$_POST['lastc'];
+			$firstc=$_POST['firstc'];
+			$company=$_POST['company'];
+			$oadd=$_POST['oadd'];
+			$tel=$_POST['tel'];
+			$email=$_POST['email'];
+			$status=$_POST['status'];
+			$notes=$_POST['notes'];
 
-		$userid = $_SESSION['id'];
-
-		$sql="INSERT INTO clients(account_id,classification,representative_first_name,representative_last_name,comaker_first_name,comaker_last_name,company_name,address,status,email,contact_number,notes)
-		  VALUES ('$userid','$class','$firstr','$lastr','$firstc','$lastc','$company','$oadd','$status','$email','$tel','$notes')";
-
-		$result = $conn->query($sql);
-
-		if(!$result){
-			echo('orayt');
-			echo $conn->error;
-		}
-		else{
-			//JOKE LANG
-			// $id = mysqli_insert_id();
-			//KELANGAN KO MAKUHA YUNG CLIENT ID PAGKAINSERT
-			$sql2="SELECT client_id FROM clients WHERE account_id='$userid' AND classification='$class' AND representative_first_name='$firstr' AND representative_last_name='$lastr' AND comaker_first_name='$firstc' AND comaker_last_name='$lastc' AND company_name='$company' AND address='$oadd' AND status='$status' AND email='$email' AND contact_number='$tel' AND notes='$notes'";
-			$result2 = mysqli_query($conn, $sql2);
-			  if (mysqli_num_rows($result2) > 0) 
-			  {
-			    while($row = mysqli_fetch_assoc($result2)) 
-			    {
-				echo('<meta http-equiv="refresh" content="0;URL=addcase.php?value='.$row['client_id'].'"/>');
-				}
+			$query1 = "INSERT INTO clients (account_id,classification,representative_first_name,representative_last_name,comaker_first_name,comaker_last_name,company_name,address,status,email,contact_number,notes)
+			  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			$stmt = $conn->prepare($query1);
+			if ($stmt) {
+				$stmt->bind_param("isssssssssss",$userid, $class, $firstr, $lastr, $firstc, $lastc, $company, $oadd, $status, $email, $tel, $notes);
+				$stmt->execute();
+				$stmt->close();
+			} else {
+				trigger_error('Statement failed : ' . $stmt->error, E_USER_ERROR);
 			}
-			else{
-				echo $conn->error;
-			}		
-		}
+
+			$query2 = "SELECT client_id FROM clients WHERE account_id=? AND classification=? AND representative_first_name=? AND representative_last_name=? AND comaker_first_name=? AND comaker_last_name=? AND company_name=? AND address=? AND status=? AND email=? AND contact_number=? AND notes=?";
+			$stmt = $conn->prepare($query2);
+
+			if ($stmt) {
+			  	$stmt->bind_param("ssssssssssss",$userid, $class, $firstr, $lastr, $firstc, $lastc, $company, $oadd, $status, $email, $tel, $notes);
+			    $stmt->execute();
+			    $stmt->bind_result($client_id);
+
+			    while ($stmt->fetch()) {
+			        echo('<meta http-equiv="refresh" content="0;URL=addcase.php?value='.$client_id.'"/>');
+			    }
+
+			    $stmt->close();
+
+			} else {
+			    trigger_error('Statement failed : ' . $stmt->error, E_USER_ERROR);
+			}
 	}
 	else{
 		echo $conn->error;
